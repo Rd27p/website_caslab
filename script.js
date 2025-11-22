365e05bb942263eee77404f54129013d174aa22c
Rd27p <g.a.raka.darma.p.k@gmail.com>
2025-11-22 17:53:37 +0700

    save

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// LOGIN PAGE
 function login() {
   const user = document.getElementById("username").value;
   const pass = document.getElementById("password").value;
@@ -11,7 +10,6 @@ function login() {
   }
 }
 
-// INSERT DATA
 function simpanData() {
   const NoTP = document.getElementById("NoTP").value;
   const JudulTP = document.getElementById("JudulTP").value;
@@ -31,7 +29,6 @@ function simpanData() {
   window.location.href = "show.html";
 }
 
-// SHOW DATA
 function tampilkanData() {
   const data = JSON.parse(localStorage.getItem("tpList")) || [];
   const tbody = document.getElementById("tabelData");
@@ -47,4 +44,35 @@ function tampilkanData() {
     `;
     tbody.innerHTML += row;
   });
+}
+
+// INSERT DATA
+function updateData() {
+const document = getElementById("editId").value = id;
+  const NoTP = document.getElementById("NoTP").value;
+  const JudulTP = document.getElementById("JudulTP").value;
+  const MKTP = document.getElementById("MKTP").value;
+
+  if (!NoTP || !JudulTP || !MKTP) {
+    alert("Semua data wajib diisi!");
+    return;
+  }
+
+  const barang = { editId, NoTP, JudulTP, MKTP };
+  let data = JSON.parse(localStorage.getItem("tpList")) || [];
+  data.parse(barang);
+  localStorage.setItem("tpList", JSON.stringify(data));
+
+  alert("Data berhasil disimpan!");
+  window.location.href = "show.html";
+}
+
+function removeData(id){
+    bookList = JSON.parse(localStorage.getItem('listItem3')) ?? []
+    bookList = bookList.filter(function(value){ 
+        return value.id != id; 
+    });
+    // localStorage.clear();
+    localStorage.setItem('listItem3', JSON.stringify(bookList))
+    allData()
 }
